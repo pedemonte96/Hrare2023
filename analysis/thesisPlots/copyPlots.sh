6f3399515cfa1ef14e87a15a21f8f89d7d9968c1
Martí <pedemonte96@gmail.com>
2023-10-31 22:36:43 -0400

    move new plots to thesis

--- analysis/thesisPlots/copyPlots.sh
+++ analysis/thesisPlots/copyPlots.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+# 4
 cp /home/submit/pdmonte/public_html/thesisPlots/*kinematic_fit_residual.png /home/submit/pdmonte/CMSSW_10_6_27/src/Hrare2023/thesis/resources/plots/
+# 2
+cp /home/submit/pdmonte/public_html/thesisPlots/*fullmeson_mass_residual.png /home/submit/pdmonte/CMSSW_10_6_27/src/Hrare2023/thesis/resources/plots/
+# 4
+cp /home/submit/pdmonte/public_html/thesisPlots/ditrack_residuals* /home/submit/pdmonte/CMSSW_10_6_27/src/Hrare2023/thesis/resources/plots/
+# 1
+cp /home/submit/pdmonte/public_html/thesisPlots/fullmeson_residuals_pt.png /home/submit/pdmonte/CMSSW_10_6_27/src/Hrare2023/thesis/resources/plots/
+# 8
+cp /home/submit/pdmonte/public_html/thesisPlots/*model_pt* /home/submit/pdmonte/CMSSW_10_6_27/src/Hrare2023/thesis/resources/plots/
+# 1 PYPLOT
+# TODO
+# 4+4+1(residuals)
+#cp /home/submit/pdmonte/public_html/thesisPlots/*mass.png /home/submit/pdmonte/CMSSW_10_6_27/src/Hrare2023/thesis/resources/plots/
+# 4
+#cp /home/submit/pdmonte/public_html/thesisPlots/*photon* /home/submit/pdmonte/CMSSW_10_6_27/src/Hrare2023/thesis/resources/plots/
+# 8
+cp /home/submit/pdmonte/public_html/thesisPlots/*sliced.png /home/submit/pdmonte/CMSSW_10_6_27/src/Hrare2023/thesis/resources/plots/
+
+
